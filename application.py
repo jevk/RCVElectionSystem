e96689bd174054edf76f672d0cc4250b6f999a90
emeraldtip <emeraldtipyt@gmail.com>
2024-03-18 20:38:33 +0200

    POST request logic + checking whether player in finland logic

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
 from flask import *
 from datetime import datetime, timedelta
 import os
+import csv
+import json
+import requests
 
 app = Flask(__name__)
+
 candidates = []
+voted_ips = []
+voted_names = []
 logfile = ""
 
 
@@ -30,6 +36,25 @@ def log(input): #we want to only log outputs from the actual voting site flask o
 
 
 
+#vote validation logic
+
+
+def check_finland_name(name): #check whether voter is a part of finland
+    names = requests.get("https://api.earthmc.net/v2/aurora/nations/Finland").json()["residents"]
+    if name in names:
+        return True
+    return False
+
+def check_voted_name(name):
+    if name not in voted_names:
+        return True
+    return False
+
+def check_voted_ip(ip):
+    if ip not in voted_ips:
+        return True
+    return False
+    
 
 #flask logic    
 
@@ -39,13 +64,27 @@ def go_to_results(e):
 
 @app.route("/vote", methods=['GET', 'POST']) #voting page
 def voting():
-    
-    #ordinal conversion oneliner, stolen from https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
-    ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4]) 
-    ordinals = [ordinal(i+1) for i in range(len(candidates))] #generate ordinals for all table rows
-    
-    return render_template("voting.html",ordinals=ordinals,candidates=candidates)
-
+    if request.method == "GET":
+        #ordinal conversion oneliner, stolen from https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
+        ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4]) 
+        ordinals = [ordinal(i+1) for i in range(len(candidates))] #generate ordinals for all table rows
+        return render_template("voting.html",ordinals=ordinals,candidates=candidates)
+    else:
+        data = request.json
+        if len(data) > 0:
+            if check_voted_ip(request.remote_addr):
+                username = data.get("voterName")
+                if check_voted_name(username):
+                    if check_finland_name(username):
+                        candidates = data.get("candidates")
+                    else:
+                        return "This username is not in a Finnish town"
+                else:
+                    return "This username has already been used to vote"
+            else:
+                return "This IP has already been used to vote"
+        else:
+            return "Invalid request"
 @app.route("/results") #results page
 def results():
     return render_template("results.html")
