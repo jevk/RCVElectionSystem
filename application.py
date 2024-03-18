85a6677fb34cb38343ed66ea4bd5d5c4e15d0053
emeraldtip <emeraldtipyt@gmail.com>
2024-03-18 21:42:48 +0200

    Validating results working

--- application.py
+++ application.py
@@ -7,10 +7,11 @@ import requests
 
 app = Flask(__name__)
 
-candidates = []
-voted_ips = []
-voted_names = []
-logfile = ""
+finns = [] #members in the nation of Finland, when the application is started
+candidates = [] #candidates in the election
+voted_ips = [] #IPs of people who already voted
+voted_names = [] #usernames of people who already voted
+logfile = "" #logfile location
 
 
 #logging logic
@@ -35,26 +36,35 @@ def log(input): #we want to only log outputs from the actual voting site flask o
         file.write(datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S ")+input+"\n")
 
 
+#csv reading/writing logic
+
+
+def write_results():
+    return
+
+def get_results():
+    return
 
 #vote validation logic
 
 
-def check_finland_name(name): #check whether voter is a part of finland
-    names = requests.get("https://api.earthmc.net/v2/aurora/nations/Finland").json()["residents"]
+def check_finland_name(name): #Returns false if person is a part of finland
     if name in names:
-        return True
-    return False
+        return False
+    return True
 
-def check_voted_name(name):
+def check_voted_name(name): #Returns false if person hasn't already voted
     if name not in voted_names:
-        return True
-    return False
+        return False
+    return True
 
-def check_voted_ip(ip):
+def check_voted_ip(ip): #Returns false if an ip hasn't already voted
     if ip not in voted_ips:
-        return True
-    return False
+        return False
+    return True
     
+    
+
 
 #flask logic    
 
@@ -72,20 +82,51 @@ def voting():
     else:
         data = request.json
         print(data)
-        if len(data) > 0:
-            if check_voted_ip(request.remote_addr):
-                username = data.get("voterName")
-                if check_voted_name(username):
-                    if check_finland_name(username):
-                        votingdata = data.get("candidates")
-                    else:
-                        return "This username is not in a Finnish town"
-                else:
-                    return "This username has already been used to vote"
-            else:
-                return "This IP has already been used to vote"
-        else:
-            return "Invalid request"
+        if len(data) == 0:
+            log(request.remote_addr + " sent a post request with no data")
+            return json.dumps({'success':False}), 418, {'ContentType':'application/json'}
+        
+        if check_voted_ip(request.remote_addr):
+            log(request.remote_addr + " tried to vote twice, raw data: " + str(data))
+            return json.dumps({'success':False,"message":"This IP has already been used to vote"}), 418, {'ContentType':'application/json'}
+
+        print(request.remote_addr)
+        username = data.get("voterName").lower()
+        if check_voted_name(username):
+            log(username + " tried to vote twice, this time with IP " + request.remote_addr + " raw data: "+ str(data))
+            return json.dumps({'success':False,"message":"This username has already been used to vote"}), 418, {'ContentType':'application/json'}
+        
+        if check_finland_name(username):
+            log(username + " tried to vote, but is not a part of Finland. IP: " + request.remote_addr + " raw data: "+ str(data))
+            return json.dumps({'success':False,"message":"This username is not in a Finnish town"}), 418, {'ContentType':'application/json'} 
+        
+        
+        voting_data = data.get("candidates")
+        parsed_output = {}
+
+        invalidated = False
+
+        for i in voting_data:
+            parsed_output[i["rank"]]=i["name"]
+        
+        if len(parsed_output) != len(candidates):
+            invalidated = True
+            
+        for i in parsed_output.values():
+            if i not in candidates:
+                invalidated = True
+
+        if len(parsed_output) != len(candidates) or broke:
+            log("How did this happen? "+username+ " at IP" + request.remote_addr + " submitted broken data: " + str(parsed_output)) 
+            return json.dumps({'success':False}), 418, {'ContentType':'application/json'}
+        
+        write_results()
+        
+            
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+
+
 @app.route("/results") #results page
 def results():
     return render_template("results.html")
@@ -107,6 +148,13 @@ if __name__ == "__main__":
             log("no candidates listed, exiting...")
             exit()
         log("Listed candidates are: "+", ".join(candidates))
+    log("Getting members in finland")
+    try:
+        names = [i.lower() for i in requests.get("https://api.earthmc.net/v2/aurora/nations/Finland").json()["residents"]]
+        log("Current members of the nation are: "+", ".join(names))
+    except:
+        log("Something went wrong with getting nation members, exiting: "+traceback.format_exc())
+        exit()
     log("Starting web application...")
     app.run()
     log("Application closed")
